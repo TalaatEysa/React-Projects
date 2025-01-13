86789e8b1952ce9aed071cb27405c045fee625d9
TalaatEysa <talaateysa@outlook.com>
2025-01-14 01:10:08 +0200

    create the layout

---
+++ 4-travel-list/src/Form.jsx
@@ -0,0 +1,7 @@
+export function Form() {
+    return (
+        <div className='add-form'>
+            <h3>What do you need for your 😍 trip?</h3>
+        </div>
+    );
+}
---
+++ 4-travel-list/src/Stats.jsx
@@ -0,0 +1,10 @@
+export function Stats() {
+    return (
+        <footer className='stats'>
+            👜
+            <em>
+                You have X items on your list, and you already packed X (X%)
+            </em>
+        </footer>
+    );
+}
