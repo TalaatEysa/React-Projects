7741e84fea4c097b81ba9a6d1ac49b1d3a29c9df
TalaatEysa <talaateysa@outlook.com>
2025-01-20 07:22:35 +0200

    [4-travel-list] calculate statistics

--- 4-travel-list/src/components/Item.jsx
+++ 4-travel-list/src/components/Item.jsx
@@ -3,7 +3,7 @@ export default function Item({ item, onDeleteItem, onToggleItem }) {
         <li>
             <input type="checkbox" value={item.packed} onChange={() => onToggleItem(item.id)} />
             <span style={{ textDecoration: item.packed ? 'line-through' : '' }}>
-                {item.description} {item.quantity}
+                {item.quantity} {item.description}
             </span>
             <button onClick={() => onDeleteItem(item.id)}>❌</button>
         </li>
