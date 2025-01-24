ad1755ce8a120ac2bacec71a22c51c7c62070a34
TalaatEysa <talaateysa@outlook.com>
2025-01-24 07:44:05 +0200

    [5-eat-n-split] building static components

---
+++ 5-eat-n-split/src/App.jsx
@@ -0,0 +1,17 @@
+import Button from './components/Button';
+import FormAddFriend from './components/FormAddFriend';
+import FormSplitBill from './components/FormSplitBill';
+import FriendsList from './components/FriendsList';
+
+export default function App() {
+    return (
+        <div className='app'>
+            <div className='sidebar'>
+                <FriendsList />
+                <FormAddFriend />
+                <Button>Add Friend</Button>
+        </div>
+        <FormSplitBill />
+        </div>
+    );
+}
