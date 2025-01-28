fb94c99f7d1bf7d7df615744f45af960c41c4477
TalaatEysa <talaateysa@outlook.com>
2025-01-28 06:32:51 +0200

    [5-eat-n-split] add new friend

--- 5-eat-n-split/src/App.jsx
+++ 5-eat-n-split/src/App.jsx
@@ -4,19 +4,47 @@ import FormAddFriend from './components/FormAddFriend';
 import FormSplitBill from './components/FormSplitBill';
 import FriendsList from './components/FriendsList';
 
+const initialFriends = [
+    {
+        id: 118836,
+        name: 'Clark',
+        image: 'https://i.pravatar.cc/48?u=118836',
+        balance: -7
+    },
+    {
+        id: 933372,
+        name: 'Sarah',
+        image: 'https://i.pravatar.cc/48?u=933372',
+        balance: 20
+    },
+    {
+        id: 499476,
+        name: 'Anthony',
+        image: 'https://i.pravatar.cc/48?u=499476',
+        balance: 0
+    }
+];
+
 export default function App() {
-  const [showAddFriend, setAddFriend] = useState(false);
-  function handleShowAddFriend() {
-    setAddFriend((show) => !show);
+    const [friends, setFriends] = useState(initialFriends);
+    const [showAddFriend, setShowAddFriend] = useState(false);
+    function handleShowAddFriend() {
+        setShowAddFriend((show) => !show);
+    }
+  function handleAddFriend(friend) {
+    setFriends((friends) => [...friends, friend]);
+    setShowAddFriend(false);
   }
     return (
         <div className='app'>
             <div className='sidebar'>
-                <FriendsList />
-                {showAddFriend && <FormAddFriend />}
-                <Button onClick={handleShowAddFriend} >{showAddFriend ? 'Close' : 'Add Friend'}</Button>
-        </div>
-        <FormSplitBill />
+                <FriendsList friends={friends} />
+                {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
+                <Button onClick={handleShowAddFriend}>
+                    {showAddFriend ? 'Close' : 'Add Friend'}
+                </Button>
+            </div>
+            <FormSplitBill />
         </div>
     );
 }
