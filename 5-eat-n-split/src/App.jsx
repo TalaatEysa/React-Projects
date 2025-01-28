03a3fb2c567919977b5410f796ae5913be64c9e1
TalaatEysa <talaateysa@outlook.com>
2025-01-28 06:20:17 +0200

    [5-eat-n-split] display new friend form

--- 5-eat-n-split/src/App.jsx
+++ 5-eat-n-split/src/App.jsx
@@ -1,15 +1,20 @@
+import { useState } from 'react';
 import Button from './components/Button';
 import FormAddFriend from './components/FormAddFriend';
 import FormSplitBill from './components/FormSplitBill';
 import FriendsList from './components/FriendsList';
 
 export default function App() {
+  const [showAddFriend, setAddFriend] = useState(false);
+  function handleShowAddFriend() {
+    setAddFriend((show) => !show);
+  }
     return (
         <div className='app'>
             <div className='sidebar'>
                 <FriendsList />
-                <FormAddFriend />
-                <Button>Add Friend</Button>
+                {showAddFriend && <FormAddFriend />}
+                <Button onClick={handleShowAddFriend} >{showAddFriend ? 'Close' : 'Add Friend'}</Button>
         </div>
         <FormSplitBill />
         </div>
