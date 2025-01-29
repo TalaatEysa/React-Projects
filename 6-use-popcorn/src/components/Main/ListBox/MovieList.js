1ff1da7a7b900e1f336836b9d6639f2c70831b17
TalaatEysa <talaateysa@outlook.com>
2025-01-29 17:20:04 +0200

    [6-use-popcorn] refactor code

---
+++ 6-use-popcorn/src/components/Main/ListBox/MovieList.js
@@ -0,0 +1,14 @@
+import Movie from './Movie';
+
+export default function MovieList({ movies }) {
+    return (
+        <ul className='list'>
+            {movies?.map((movie) => (
+                <Movie
+                    key={movie.imdbID}
+                    movie={movie}
+                />
+            ))}
+        </ul>
+    );
+}
