b903107210f2d0f9f3a9bd5c3e6f905fddbcc1f0
TalaatEysa <talaateysa@outlook.com>
2025-02-25 03:01:59 +0200

    [8-react-quiz] start new quiz

--- 8-react-quiz/src/App.jsx
+++ 8-react-quiz/src/App.jsx
@@ -5,6 +5,7 @@ import { useReducer } from 'react';
 import Loader from './components/Loader';
 import Error from './components/Error';
 import StartScreen from './components/StartScreen';
+import Question from './components/Question';
 
 const initialState = {
     questions: [],
@@ -24,6 +25,12 @@ function reducer(state, action) {
             return {
                 ...state,
                 status: 'error'
+            };
+
+        case 'start':
+            return {
+                ...state,
+                status: 'active'
             };
         default:
             throw new Error('Unknown action');
@@ -46,7 +53,13 @@ function App() {
             <Main>
                 {status === 'loading' && <Loader />}
                 {status === 'error' && <Error />}
-                {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
+                {status === 'ready' && (
+                    <StartScreen
+                        numQuestions={numQuestions}
+                        dispatch={dispatch}
+                    />
+                )}
+                {status === 'active' && <Question />}
             </Main>
         </div>
     );
--- 8-react-quiz/src/components/StartScreen.jsx
+++ 8-react-quiz/src/components/StartScreen.jsx
@@ -1,9 +1,14 @@
-export default function StartScreen({ numQuestions }) {
+export default function StartScreen({ numQuestions, dispatch }) {
     return (
         <div className='start'>
             <h2>Welcome to the React Quiz!</h2>
             <h3>{numQuestions} questions to test your React mastery</h3>
-            <button className='btn btn-ui'>Start Quiz</button>
+            <button
+                className='btn btn-ui'
+                onClick={() => dispatch({ type: 'start' })}
+            >
+                Start Quiz
+            </button>
         </div>
     );
 }
