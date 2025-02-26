d9fb4530a6b79f50319e44419b4e4aea2cf60ca5
TalaatEysa <talaateysa@outlook.com>
2025-02-26 04:59:30 +0200

    [9-worldwise] add navbar

--- 9-worldwise/src/pages/PageNotFound.jsx
+++ 9-worldwise/src/pages/PageNotFound.jsx
@@ -1,5 +1,7 @@
+import PageNav from "../components/PageNav";
+
 export default function PageNotFound() {
   return (
-    <div>PageNotFound :(</div>
+    <div><PageNav />PageNotFound :(</div>
   )
 }
