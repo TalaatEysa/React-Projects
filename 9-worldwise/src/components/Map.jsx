2707e99570b2743359048dd048b6114e429ddc4d
TalaatEysa <talaateysa@outlook.com>
2025-02-28 01:07:23 +0200

    [9-worldwise] display markers on map

--- 9-worldwise/src/components/Map.jsx
+++ 9-worldwise/src/components/Map.jsx
@@ -2,17 +2,17 @@ import { useNavigate, useSearchParams } from 'react-router-dom';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import styles from './Map.module.css';
 import { useState } from 'react';
+import { useCities } from '../context/CitiesContext';
 
 export default function Map() {
     const [searchParams, setSearchParams] = useSearchParams();
+    const { cities } = useCities();
     const [mapPosition, setMapPosition] = useState([40, 0]);
     const navigate = useNavigate();
     const lat = searchParams.get('lat');
     const lng = searchParams.get('lng');
     return (
-        <div
-            className={styles.mapContainer}
-        >
+        <div className={styles.mapContainer}>
             <MapContainer
                 center={mapPosition}
                 zoom={13}
@@ -23,12 +23,23 @@ export default function Map() {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
                 />
-                <Marker position={mapPosition}>
-                    <Popup>
-                        A pretty CSS3 popup. <br />
-                        Easily customizable.
-                    </Popup>
-                </Marker>
+                {cities.map((city) => (
+                    <Marker
+                        position={[city.position.lat, city.position.lng]}
+                        key={city.id}
+                    >
+                        <Popup>
+                            <span>
+                                <img
+                                    src={city.emoji}
+                                    alt='❌'
+                                    width='30'
+                                />
+                            </span>
+                            <span>{city.cityName}</span>
+                        </Popup>
+                    </Marker>
+                ))}
             </MapContainer>
         </div>
     );
