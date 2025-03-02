de9dce9adc015c66a313edb46d5443252ee6ea63
TalaatEysa <talaateysa@outlook.com>
2025-03-02 02:21:41 +0200

    [10-atomic-blog] use useCallback to memo functions

--- 10-atomic-blog/src/App-memo.js
+++ 10-atomic-blog/src/App-memo.js
@@ -1,4 +1,4 @@
-import { memo, useEffect, useMemo, useState } from 'react';
+import { memo, useCallback, useEffect, useMemo, useState } from 'react';
 import { faker } from '@faker-js/faker';
 
 function createRandomPost() {
@@ -25,9 +25,9 @@ function App() {
               )
             : posts;
 
-    function handleAddPost(post) {
+    const handleAddPost= useCallback(function handleAddPost(post) {
         setPosts((posts) => [post, ...posts]);
-    }
+    }, []);
 
     function handleClearPosts() {
         setPosts([]);
@@ -67,7 +67,7 @@ function App() {
                 posts={searchedPosts}
                 onAddPost={handleAddPost}
             />
-            <Archive ArchiveOptions={ArchiveOptions} />
+            <Archive ArchiveOptions={ArchiveOptions} onAddPost={handleAddPost} setIsFakeDark={setIsFakeDark} />
             <Footer />
         </section>
     );
@@ -164,7 +164,7 @@ function List({ posts }) {
     );
 }
 
-const Archive = memo(function Archive({ ArchiveOptions }) {
+const Archive = memo(function Archive({ ArchiveOptions,onAddPost }) {
     // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
     const [posts] = useState(() =>
         // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
@@ -187,7 +187,7 @@ const Archive = memo(function Archive({ ArchiveOptions }) {
                             <p>
                                 <strong>{post.title}:</strong> {post.body}
                             </p>
-                            {/* <button onClick={() => onAddPost(post)}>Add as new post</button> */}
+                            <button onClick={() => onAddPost(post)}>Add as new post</button>
                         </li>
                     ))}
                 </ul>
