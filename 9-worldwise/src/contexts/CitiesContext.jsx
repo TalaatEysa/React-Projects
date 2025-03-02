afc36eac33f90703fa47f9441bb1054e008120e3
TalaatEysa <talaateysa@outlook.com>
2025-03-02 02:46:59 +0200

    [9-worldwise] fix City component by using useCallback

--- 9-worldwise/src/contexts/CitiesContext.jsx
+++ 9-worldwise/src/contexts/CitiesContext.jsx
@@ -1,3 +1,4 @@
+import { useCallback } from 'react';
 import { useEffect, useReducer, createContext, useContext } from 'react';
 
 const BASE_URL = 'http://localhost:8000';
@@ -75,20 +76,23 @@ function CitiesProvider({ children }) {
         fetchCities();
     }, []);
 
-    async function getCity(id) {
-        if (Number(id) === currentCity.id) return;
-        dispatch({ type: 'loading' });
-        try {
-            const res = await fetch(`${BASE_URL}/cities/${id}`);
-            const data = await res.json();
-            dispatch({ type: 'city/loaded', payload: data });
-        } catch {
-            dispatch({
-                type: 'rejected',
-                payload: 'There was an error loading city'
-            });
-        }
-    }
+    const getCity = useCallback(
+        async function getCity(id) {
+            if (Number(id) === currentCity.id) return;
+            dispatch({ type: 'loading' });
+            try {
+                const res = await fetch(`${BASE_URL}/cities/${id}`);
+                const data = await res.json();
+                dispatch({ type: 'city/loaded', payload: data });
+            } catch {
+                dispatch({
+                    type: 'rejected',
+                    payload: 'There was an error loading city'
+                });
+            }
+        },
+        [currentCity.id]
+    );
 
     async function createCity(newCity) {
         dispatch({ type: 'loading' });
