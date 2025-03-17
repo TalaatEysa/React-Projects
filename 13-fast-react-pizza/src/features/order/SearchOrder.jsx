c6bbc631cdfe9f3a21d8b689a2ff66b83e79da5a
TalaatEysa <talaateysa@outlook.com>
2025-03-17 05:37:09 +0200

    [13-fast-react-pizza] style components using tailwind

--- 13-fast-react-pizza/src/features/order/SearchOrder.jsx
+++ 13-fast-react-pizza/src/features/order/SearchOrder.jsx
@@ -1,24 +1,25 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function SearchOrder() {
-    const [query, setQuery] = useState('');
-    const navigate = useNavigate();
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
 
-    function handeleSubmit(e) {
-        e.preventDefault();
-        if (!query) return;
-        navigate(`/order/${query}`);
-        setQuery('');
-    }
-    return (
-        <form onSubmit={handeleSubmit}>
-            <input
-                type='text'
-                placeholder='Search order #'
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-            />
-        </form>
-    );
+  function handeleSubmit(e) {
+    e.preventDefault();
+    if (!query) return;
+    navigate(`/order/${query}`);
+    setQuery("");
+  }
+  return (
+    <form onSubmit={handeleSubmit}>
+      <input
+        type="text"
+        placeholder="Search order #"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-slate-400 sm:focus:w-72 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64"
+      />
+    </form>
+  );
 }
--- 13-fast-react-pizza/tailwind.config.js
+++ 13-fast-react-pizza/tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-    theme: {
-        extend: {}
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
     },
-    plugins: []
+    extend: {
+      colors: {
+        pizza: "#123456",
+      },
+      height: {
+        screen: "100dvh",
+      },
+    },
+  },
+  plugins: [],
 };
