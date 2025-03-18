a76a7a1343aa8ab7c1169fa7f7f3d79bcb7aa43d
TalaatEysa <talaateysa@outlook.com>
2025-03-18 05:48:20 +0200

    [13-fast-react-pizza] read and update user state

--- 13-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 13-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -34,6 +35,7 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+    const username = useSelector((state) => state.user.username);
     const navigation = useNavigation();
     const isSubmitting = navigation.state === "submitting";
     const formErrors = useActionData();
@@ -55,6 +57,7 @@ function CreateOrder() {
                         type="text"
                         name="customer"
                         required
+                        defaultValue={username}
                     />
                 </div>
 
