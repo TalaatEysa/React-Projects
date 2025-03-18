d51a799afb64eaaab762e2faffee2938d3e00545
TalaatEysa <talaateysa@outlook.com>
2025-03-18 06:07:06 +0200

    [13-fast-react-pizza] add item to cart

--- 13-fast-react-pizza/src/features/menu/MenuItem.jsx
+++ 13-fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -1,34 +1,51 @@
+import { useDispatch } from "react-redux";
 import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
+import { addItem } from "../cart/cartSlice";
 
 function MenuItem({ pizza }) {
-  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+    const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+    const dispatch = useDispatch();
 
-  return (
-    <li className="flex gap-4 py-2">
-      <img
-        src={imageUrl}
-        alt={name}
-        className={`h-24 ${soldOut ? "opacity-70 grayscale" : ""}`}
-      />
-      <div className="flex grow flex-col pt-0.5">
-        <p className="font-medium">{name}</p>
-        <p className="text-sm capitalize italic text-slate-500">
-          {ingredients.join(", ")}
-        </p>
-        <div className="mt-auto flex items-center justify-between">
-          {!soldOut ? (
-            <p className="text-sm">{formatCurrency(unitPrice)}</p>
-          ) : (
-            <p className="text-sm font-medium uppercase text-slate-500">
-              Sold out
-            </p>
-          )}
-          <Button type="small">Add to cart</Button>
-        </div>
-      </div>
-    </li>
-  );
+    function handleAddToCart() {
+        const newItem = {
+            pizzaId: id,
+            name,
+            quantity: 1,
+            unitPrice,
+            totalPrice: unitPrice * 1,
+        };
+        dispatch(addItem(newItem));
+    }
+    return (
+        <li className="flex gap-4 py-2">
+            <img
+                src={imageUrl}
+                alt={name}
+                className={`h-24 ${soldOut ? "opacity-70 grayscale" : ""}`}
+            />
+            <div className="flex grow flex-col pt-0.5">
+                <p className="font-medium">{name}</p>
+                <p className="text-sm capitalize italic text-slate-500">
+                    {ingredients.join(", ")}
+                </p>
+                <div className="mt-auto flex items-center justify-between">
+                    {!soldOut ? (
+                        <p className="text-sm">{formatCurrency(unitPrice)}</p>
+                    ) : (
+                        <p className="text-sm font-medium uppercase text-slate-500">
+                            Sold out
+                        </p>
+                    )}
+                    {!soldOut && (
+                        <Button type="small" onClick={handleAddToCart}>
+                            Add to cart
+                        </Button>
+                    )}
+                </div>
+            </div>
+        </li>
+    );
 }
 
 export default MenuItem;
