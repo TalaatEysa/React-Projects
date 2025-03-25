ec17163362423f56431903acee8bc2d247c20116
TalaatEysa <talaateysa@outlook.com>
2025-03-25 02:27:43 +0200

    [14-the-wild-oasis] set up and use supabase as backend

--- 14-the-wild-oasis/.eslintrc.cjs
+++ 14-the-wild-oasis/.eslintrc.cjs
@@ -17,6 +17,7 @@ module.exports = {
             { allowConstantExport: true }
         ],
         'react/prop-types': 'off',
-        'no-unused-vars': 'warn'
+        'no-unused-vars': 'warn',
+        'no-undef': 'warn',
     }
 };
