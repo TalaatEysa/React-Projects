bc020d5fcba1001a0733cac006bfd7e39ebf54ac
TalaatEysa <talaateysa@outlook.com>
2025-04-02 02:56:48 +0200

    [14-the-wild-oasis] apply render props pattern

--- 14-the-wild-oasis/src/ui/Table.jsx
+++ 14-the-wild-oasis/src/ui/Table.jsx
@@ -74,7 +74,8 @@ function Header({ children }) {
     return (
         <StyledHeader
             role='row'
-            columns={columns} as='header'
+            columns={columns}
+            as='header'
         >
             {children}
         </StyledHeader>
@@ -91,8 +92,10 @@ function Row({ children }) {
         </StyledRow>
     );
 }
-function Body({ children }) {
-    const { columns } = useContext(TableContext);
+function Body({ data, render }) {
+    if (!data.length) return <Empty>No data to show at the moment</Empty>;
+
+    return <StyledBody>{data.map(render)}</StyledBody>;
 }
 
 Table.Header = Header;
