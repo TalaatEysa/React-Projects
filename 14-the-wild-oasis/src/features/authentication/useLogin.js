b275b131bc4cbadb098f5b852b35c8a1ade5fb16
TalaatEysa <talaateysa@outlook.com>
2025-04-17 00:02:37 +0200

    [14-the-wild-oasis] add authentication and login user

---
+++ 14-the-wild-oasis/src/features/authentication/useLogin.js
@@ -0,0 +1,20 @@
+import { useMutation } from '@tanstack/react-query';
+import { login as loginApi } from '../../services/apiAuth';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-hot-toast';
+
+export function useLogin() {
+    const navigate = useNavigate();
+
+    const { isLoading, mutate: login } = useMutation({
+        mutationFn: ({ email, password }) => loginApi({ email, password }),
+        onSuccess: (user) => {
+            navigate('/dashboard');
+        },
+        onError: (err) => {
+            console.log('ERROR', err);
+            toast.error('Provided email or password is incorrect');
+        }
+    });
+    return { isLoading, login };
+}
