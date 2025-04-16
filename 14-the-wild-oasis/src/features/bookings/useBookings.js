a83dab089c67b9554a37cfaed296f3b08700fd97
TalaatEysa <talaateysa@outlook.com>
2025-04-16 19:33:58 +0200

    [14-the-wild-oasis] use react query to prefetch in bookings table

--- 14-the-wild-oasis/src/features/bookings/useBookings.js
+++ 14-the-wild-oasis/src/features/bookings/useBookings.js
@@ -1,15 +1,20 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { useSearchParams } from 'react-router-dom';
 import { getBookings } from '../../services/apiBookings';
+import { PAGE_SIZE } from '../../utils/constants';
 
 export function useBookings() {
+    const queryClient = useQueryClient();
     const [searchParams] = useSearchParams();
 
+    // Filter
     const filterValue = searchParams.get('status');
     const filter =
         !filterValue || filterValue === 'all'
             ? null
             : { field: 'status', value: filterValue };
+
+    // SortBy
     const sortByRaw = searchParams.get('sortBy') || 'startDate-asc';
     const [field, direction] = sortByRaw.split('-');
     const sortBy = { field, direction };
@@ -25,6 +30,21 @@ export function useBookings() {
         queryKey: ['bookings', filter, sortBy, page],
         queryFn: () => getBookings({ filter, sortBy, page })
     });
+
+    // prefetch next page
+    const pageCount = Math.ceil(count / PAGE_SIZE);
+    if (page < pageCount) {
+        queryClient.prefetchQuery({
+            queryKey: ['bookings', filter, sortBy, page + 1],
+            queryFn: () => getBookings({ filter, sortBy, page: page + 1 })
+        });
+    }
+    if (page > 1) {
+        queryClient.prefetchQuery({
+            queryKey: ['bookings', filter, sortBy, page + 1],
+            queryFn: () => getBookings({ filter, sortBy, page: page + 1 })
+        });
+    }
 
     return {
         bookings,
