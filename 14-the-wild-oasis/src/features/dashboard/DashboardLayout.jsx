f14e3ca2acd61efdb0c9e3655a7aad6a8cd290bb
TalaatEysa <talaateysa@outlook.com>
2025-04-29 05:48:07 +0300

    [14-the-wild-oasis] display a pie chart for duration using recharts

--- 14-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ 14-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -5,6 +5,7 @@ import { useRecentStays } from './useRecentStays';
 import Stats from './Stats';
 import { useCabins } from './../cabins/useCabins';
 import SalesChart from './SalesChart';
+import DurationChart from './DurationChart';
 
 const StyledDashboardLayout = styled.div`
     display: grid;
@@ -37,7 +38,7 @@ export default function DashboardLayout() {
                 cabinCount={cabins.length}
             />
             <div>Today&apos;s Activity</div>
-            <div>Chart stay durations</div>
+            <DurationChart confirmedStays={confirmedStays} />
             <SalesChart
                 bookings={bookings}
                 numDays={numDays}
