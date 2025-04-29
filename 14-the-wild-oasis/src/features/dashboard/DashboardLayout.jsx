f77ce3da74d47c85c373f6a84774979348d4923b
TalaatEysa <talaateysa@outlook.com>
2025-04-29 06:19:08 +0300

    [14-the-wild-oasis] display today stays

--- 14-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ 14-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import Stats from './Stats';
 import { useCabins } from './../cabins/useCabins';
 import SalesChart from './SalesChart';
 import DurationChart from './DurationChart';
+import TodayActivity from '../check-in-out/TodayActivity';
 
 const StyledDashboardLayout = styled.div`
     display: grid;
@@ -18,7 +19,6 @@ export default function DashboardLayout() {
     const { isLoading: isLoadingBookings, bookings } = useRecentBookings();
 
     const {
-        stays,
         isLoading: isLoadingStays,
         confirmedStays,
         numDays
@@ -37,7 +37,7 @@ export default function DashboardLayout() {
                 numDays={numDays}
                 cabinCount={cabins.length}
             />
-            <div>Today&apos;s Activity</div>
+            <TodayActivity />
             <DurationChart confirmedStays={confirmedStays} />
             <SalesChart
                 bookings={bookings}
